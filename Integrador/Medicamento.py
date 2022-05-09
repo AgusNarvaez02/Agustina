be5d51fe86dd97dad569febeac888733cd5da2ac
AgusNarvaez02 <103900236+AgusNarvaez02@users.noreply.github.com>
2022-05-08 23:08:34 -0300

    Create Medicamento.py

---
+++ Integrador/Medicamento.py
@@ -0,0 +1,34 @@
+class Medicamento :
+    __idCama: int
+    __idMedicamento: int
+    __nom= ''
+    __monodroga=''
+    __presentacion= ''
+    __cant= ''
+    __total: float
+    
+    def __init__(self, idCama, idMedicamento, nom, monodroga, presentacion, cant, total):
+        self.__idCama= int(idCama)
+        self.__idMedicamento= int(idMedicamento)
+        self.__nom= nom
+        self.__monodroga= monodroga
+        self.__presentacion= presentacion
+        self.__cant= cant
+        self.__total= float(total)
+    def getidCam (self):
+        return self.__idCama
+    def getidMed (self):
+        return self.__idMedicamento
+    
+    def getnom(self):
+        return self.__nom
+    def getmonod(self):
+        return self.__monodroga
+    
+    def getpres(self):
+        return self.__presentacion
+    def getcant(self):
+        return self.__cant
+    def getot(self):
+        return self.__total
+    
